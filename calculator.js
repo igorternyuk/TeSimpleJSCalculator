796e8c09c3dc3bbb71a267c774dcd52ab432b78f
igorternyuk <mr.ternyuk@mail.ru>
2018-04-14 18:38:27 +0300

    second commit

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+const PI = Math.PI;
+const E = Math.E;
+
 Array.prototype.contains = function(obj) {
     for(var i = 0; i < this.lenth; ++i){
     	if(this[i] === obj){
@@ -30,5 +33,46 @@ function onBtnDotClicked(){
 }
 
 function onBtnClearClicked(){
-	document.getElementById("display").value = "0";
+	document.getElementById("display").value = "";
+}
+
+function onKeyReleased(event) {
+    var keyCode = event.which || event.keyCode;
+    if(keyCode === 13){
+    	onBtnEqualsClicked();
+    } else if( keyCode === 27){
+    	onBtnClearClicked();
+    }
+}
+
+function sin(x){
+	return Math.sin(x);
+}
+
+function cos(x){
+	return Math.cos(x);
+}
+
+function tan(x){
+	return Math.tan(x);
+}
+
+function exp(x){
+	return Math.exp(x);
+}
+
+function log(x){
+	return Math.log(x);
+}
+
+function log10(x){
+	return Math.log10(x);
+}
+
+function sqrt(x){
+	return Math.sqrt(x);
+}
+
+function cbrt(x){
+	return Math.cbrt(x);
 }
